4e273cf57fc0dad787fc4fc17c386db7079a3399
mkery <mkery@cs.cmu.edu>
2018-04-12 21:40:22 -0400

    fixes

--- js/huggabug.js
+++ js/huggabug.js
@@ -2,38 +2,52 @@
 
 class Huggabug {
 
-	constructor(x, y) 
+	constructor(x, y, ctx, depth, image) 
 	{
+		this.ctxDepth = depth
+		this.ctx = ctx
 		this.x = x
 		this.y = y
+		this.angle = 1
+		this.image = image
 	}
 
-	buildElements()
+	draw(width)
 	{
-		this.bug = $('<div/>').addClass('huggabug')
-		this.bug.appendTo($('.particleMap'))
-		this.resizeTo(this.x, this.y)
-		this.moveTo(this.x, this.y)
+		if(!width)
+		{
+			var data = this.ctxDepth.getImageData(this.x, this.y, 1, 1).data
+			var depth = 1 - (data[0]/255)
+			var width = (depth*10)*(depth*10)
+		}
+
+		//console.log('DEPTH', data[0], depth, width)
+
+		//this.ctx.fillRect(this.x, this.y, width, width)
+		this.ctx.drawImage(this.image, this.x, this.y, width, width )
 	}
 
-	resizeTo(x, y)
+	move()
 	{
+		this.ctx.resetTransform()
+		this.ctx.translate(this.x, this.y)
+
+
+		var angle = 2 
+		this.angle = this.angle + Math.pow(Math.ceil(Math.random()*angle), 2) - Math.pow(Math.ceil(Math.random()*angle), 2)
+		this.ctx.rotate(this.angle*Math.PI/180)
+
+		var data = this.ctxDepth.getImageData(this.x, this.y, 1, 1).data
+		var depth = 1 - (data[0]/255)
+		var width = (depth*10)*(depth*10)
+
+		var move = 2
 		
+		this.x = this.x + depth*(Math.pow(Math.ceil(Math.random()*move), 2) - Math.pow(Math.ceil(Math.random()*move), 2))
+		this.y = this.y + depth*(Math.pow(Math.ceil(Math.random()*move), 2) - Math.pow(Math.ceil(Math.random()*move), 2))
+
+		this.draw(width)
+		this.ctx.resetTransform()
 	}
-
-	moveTo(x, y)
-	{
-		var maxX = $('.painting').width()
-		var maxY = $('.painting').height()
-		var width = $(this.bug).width()
-		var height = width
-
-		if(x + width > maxX)
-			x = maxX - width
-		if(y + height > maxY)
-			y = maxY - height
-
-		this.bug.css('top', y+'px').css('left', x+'px')
-	}	
 
 }
--- js/index.js
+++ js/index.js
@@ -1,30 +1,53 @@
 
+
 $(document).ready(function(){
 
 	var bugsCount = 100
 	var width = $('.painting').width()
 	var height = $('.painting').height()
+	var bugs = []
+
+	var canvas = $('.particleMap')[0]
+	canvas.width= width;
+	canvas.height= height;
+	var ctxDraw = canvas.getContext("2d");
+
+
+	var butterby = new Image(width, height)
+    butterby.src = './images/butterby.png'
+    butterby.crossOrigin = "anonymous"
+
 
 	var depthMap = new Image(width, height)
     depthMap.src = './art/ballet_depth.png'
     depthMap.crossOrigin = "anonymous"
-    depthMap.onload = () => {
+
+    redraw = function()
+	{
+		ctxDraw.resetTransform()
+		ctxDraw.clearRect(0, 0, width, height)
+		bugs.forEach((item, index) => {
+			item.move()
+		})
+		setTimeout(() => redraw(), 100)
+	}
+
+    butterby.onload = () => {
 		var offScreenCanvas= document.createElement('canvas');
-		//$('body').append($(offScreenCanvas))
 	    offScreenCanvas.width= width;
 	    offScreenCanvas.height= height;
-	    var context= offScreenCanvas.getContext("2d");
-	    context.drawImage(depthMap, 0, 0, width, height)
-	    var imgd = context.getImageData(0, 0, width, height);
-		var pix = imgd.data;
-		console.log(pix)
+	    var ctxDepth= offScreenCanvas.getContext("2d");
+	    ctxDepth.drawImage(depthMap, 0, 0, width, height)
 
 		for(var i = 0; i < bugsCount; i++)
 		{
 			var x = Math.ceil(Math.random()*width)
 			var y = Math.ceil(Math.random()*height)
-			var hb = new Huggabug(x,y)
-			hb.buildElements()
+			var hb = new Huggabug(x,y, ctxDraw, ctxDepth, butterby)
+			hb.draw()
+			bugs.push(hb)
 		}
+
+		setTimeout(() => redraw(), 1000)
     }
 })
